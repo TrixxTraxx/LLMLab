9ddc98fb2f004a5810c9f4688051774b291cb45a
trixxtraxx <kosimo.weiss@gmail.com>
2025-06-17 12:34:16 +0200

    ADD: Better Frontend Compression

--- LLMLab.ClientWebServer/Program.cs
+++ LLMLab.ClientWebServer/Program.cs
@@ -1,7 +1,11 @@
 // MinimalFileServer/Program.cs
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddResponseCompression();
+
 var app = builder.Build();
+
+app.UseResponseCompression();
 
 app.UseStaticFiles(new StaticFileOptions()
 {
